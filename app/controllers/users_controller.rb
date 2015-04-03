39a60fa7a23dc4ca1579e17d65d8741d449cdc0b
Michael Pope <mpope.cr@gmail.com>
2015-04-02 23:31:39 -0400

    Add redirect & strong params for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    redirect_to email_sent_path(user)
   end
 
   def new
     @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
