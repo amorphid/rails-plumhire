1cf3e8eb35d631948d3194435f1d5cbf53b3696d
Michael Pope <mpope.cr@gmail.com>
2015-04-02 22:45:42 -0400

    Add route test for POST /users

---
+++ spec/routing/users_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe "User routes" do
+
+  it "POST /users"do
+    expect(post: "/users").to route_to(
+      controller: "users",
+      action:     "create"
+    )
+  end
+end
