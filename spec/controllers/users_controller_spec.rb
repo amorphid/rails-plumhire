c3f992eae7f59e6116bcc393f1487761cc6a7f48
Michael Pope <mpope.cr@gmail.com>
2015-04-02 23:03:27 -0400

    Add test for UsersController#create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 describe UsersController do
-  describe "#new" do
+  context "#create" do
+    it "redirects  w/ valid input" do
+      user = Fabricate.attributes_for(:user)
+      post :create, user: user
+      expect(response).to redirect_to(email_sent_path)
+    end
+  end
+
+  context "#new" do
     it "assigns @user" do
       get :new
       expect(assigns[:user]).to be_instance_of(User)
