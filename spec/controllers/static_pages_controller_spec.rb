ba76bb691e6ca2f38bbe1ab269a493e88e956fba
Michael Pope <mpope.cr@gmail.com>
2015-04-04 20:37:54 -0400

    Add 2 tests for sign in flash message

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -8,4 +8,16 @@ describe StaticPagesController do
       expect(assigns[:user]).to eq(user)
     end
   end
+
+  context "#sign_in" do
+    it "sets flash success message on email confirmation" do
+      get :sign_in, email: Faker::Internet.email, email_confirmed: true
+      expect(flash[:success]).to be_present
+    end
+
+    it "does not set flash success message w/o email confirmation" do
+      get :sign_in
+      expect(flash[:success]).not_to be_present
+    end
+  end
 end
