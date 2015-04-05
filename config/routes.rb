e88cec86dfae53484174260721e7cb05ecf13b95
Michael Pope <mpope.cr@gmail.com>
2015-04-04 22:23:10 -0400

    Implement route for GET /home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/email_sent/:id", to: "static_pages#email_sent", as: "email_sent"
+  get "/home", to: "static_pages#home"
   get "/sign_in", to: "static_pages#sign_in"
   get "/sign_up", to: "static_pages#sign_up"
   post "/sign_in", to: "static_pages#sign_in_post"
