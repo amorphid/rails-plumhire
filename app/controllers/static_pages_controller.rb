edaa19ad8a105d49065c28843f046e1bcd1dfff4
Michael Pope <mpope.cr@gmail.com>
2015-04-04 20:32:56 -0400

    Implement flash message for email confirmation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,10 @@ class StaticPagesController < ApplicationController
   def email_sent
     @user = User.find(params[:id])
   end
+
+  def sign_in
+    if params[:email_confirmed]
+      flash[:success] = "Email #{params[:email]} confirmed"
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,13 +4,13 @@ describe UsersController do
   context "#create" do
     let(:user_attr) { Fabricate.attributes_for(:user) }
 
-    it "sends an email" do
+    it "sends an email w/ valid input" do
       pre_count = ActionMailer::Base.deliveries.count
       post :create, user: user_attr
       expect(ActionMailer::Base.deliveries.count).to eq(pre_count + 1)
     end
 
-    it "redirects  w/ valid input" do
+    it "redirects w/ valid input" do
       post :create, user: user_attr
       user = User.find_by(email: user_attr[:email])
       expect(response).to redirect_to(email_sent_path(user))
