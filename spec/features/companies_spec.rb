bb54d2061e378c7bbf11b777629d721fd2d369e0
Michael Pope <mpope.cr@gmail.com>
2015-04-06 18:19:17 -0400

    Add feature test for company creation

---
+++ spec/features/companies_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+feature "Company" do
+  context "creation" do
+    let(:company)  { Fabricate(:company, name: Faker::Company.name) }
+    let(:password) { Faker::Internet.password }
+    let(:user)     { Fabricate(:user, password: password) }
+
+    scenario "succeeds w/ valid input" do
+      visit sign_in_path
+      fill_in :email, with: user.email
+      fill_in :password, with: password
+      click_button("Submit")
+      click_link("New company")
+      fill_in :company_name, with: company.name
+      click_button("Submit")
+      expect(page).to have_content(company.name)
+    end
+  end
+end
