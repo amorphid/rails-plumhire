59c5b310c51e31781f181208d914db32342683af
Michael Pope <mpope.cr@gmail.com>
2015-04-06 10:24:02 -0400

    Add error handling for edge cases where session[:can] is nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authorize!(action)
+    # because authorization actions are stored in session[:can]
+    # the following line provice a default value
+    # this prevents getting nil errors
+    # example:
+    # session[:can][action] throws error if session[:can] is nil
+    session[:can] ||= {}
+
     redirect_to home_path unless session[:can][action]
   end
 end
