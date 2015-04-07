c3fb6ea749d926e6c61d05f68c61045f2b41441d
Michael Pope <mpope.cr@gmail.com>
2015-04-06 23:02:14 -0400

    Add route test for GET /companies/:id

--- spec/routing/companies_controller_spec.rb
+++ spec/routing/companies_controller_spec.rb
@@ -1,6 +1,14 @@
 require "rails_helper"
 
 describe CompaniesController do
+  it "GET /companies/:id" do
+    expect(get: "/companies/:id").to route_to(
+      controller: "companies",
+      id:         ":id"
+      action:     "show"
+    )
+  end
+
   it "GET /companies/new" do
     expect(get: "/companies/new").to route_to(
       controller: "companies",
