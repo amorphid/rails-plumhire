40ed8e3aaa99c1dfd7103ef66cbcd2885a215099
Michael Pope <mpope.cr@gmail.com>
2015-04-06 22:47:12 -0400

    Add authorization to CompaniesController

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,10 @@
 class CompaniesController < ApplicationController
+  before_filter :authorize_user!
+
   def create
   end
 
   def new
-    authorize_user!
-
     @company = Company.new
   end
 end
