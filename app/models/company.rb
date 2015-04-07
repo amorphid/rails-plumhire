61a3da5b474ebb7682adbb56bae794a879c3e746
Michael Pope <mpope.cr@gmail.com>
2015-04-07 11:52:24 -0400

    Add user_id to Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :user_id, presence: true
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,6 +3,10 @@ require "rails_helper"
 describe Company do
   context "#name" do
     it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
+
+  end
+
+  context "#user_id" do
+    it { should validate_presence_of(:user_id) }
   end
 end
