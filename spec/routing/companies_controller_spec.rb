fcce8c6618106fd9ad8fe88b5138236a39c36a96
Michael Pope <mpope.cr@gmail.com>
2015-04-06 22:38:31 -0400

    Add routing test for POST /companies

--- spec/routing/companies_controller_spec.rb
+++ spec/routing/companies_controller_spec.rb
@@ -7,4 +7,11 @@ describe CompaniesController do
       action:     "new"
     )
   end
+
+  it "POST /companies" do
+    expect(post: "/companies").to route_to(
+      controller: "companies",
+      action:     "create"
+    )
+  end
 end
