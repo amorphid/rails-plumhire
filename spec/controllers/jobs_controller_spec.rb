9a1f3b76acfa1a87db0122f33209ce844787343c
Michael Pope <mpope.cr@gmail.com>
2015-04-07 17:50:44 -0400

    Add 2 controller tests for JobsController#new

---
+++ spec/controllers/jobs_controller_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe JobsController do
+  context "#new" do
+    let(:company) { Fabricate(:company) }
+    let(:user) { company.user }
+
+    it "assigns @company" do
+      session[:user_id] = user.id
+      get :new, company_id: company.id
+      expect(assigns[:company]).to be_instance_of(Company)
+    end
+
+    it "redirects if not signed in" do
+      session[:user_id] = nil
+      get :new, company_id: company.id
+      expect(response).to redirect_to(sign_in_path)
+    end
+  end
+end
