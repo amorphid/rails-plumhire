52735d48ff9e126929f1b9f13cf3d29c3b313ae5
Michael Pope <mpope.cr@gmail.com>
2015-04-08 09:36:49 -0400

    Flush out JobsController#create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,11 @@
 class JobsController < ApplicationController
+  def create
+    authorize_user!
+    job = Job.new(company_id: params[:company_id])
+    job.save
+    redirect_to company_job_path(job)
+  end
+
   def new
     authorize_user!
     @job = Job.new(company_id: params[:company_id])
--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,10 +1,25 @@
 require "rails_helper"
 
 describe JobsController do
-  context "#new" do
-    let(:company) { Fabricate(:company) }
-    let(:user) { company.user }
+  let(:company) { Fabricate(:company) }
+  let(:user) { company.user }
 
+  context "#create" do
+    it "creates a Job" do
+      pre_count = Job.count
+      session[:user_id] = user.id
+      post :create, company_id: company.id, company: company.attributes
+      expect(Job.count).to eq(Job.count + 1)
+    end
+
+    it "redirects if not signed in" do
+      session[:user_id] = nil
+      post :create, company_id: company.id
+      expect(response).to redirect_to(sign_in_path)
+    end
+  end
+
+  context "#new" do
     it "assigns @company" do
       session[:user_id] = user.id
       get :new, company_id: company.id
