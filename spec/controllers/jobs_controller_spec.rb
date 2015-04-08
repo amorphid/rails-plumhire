f56af4ad1ce1f71deb8a72d205b30aca66571ae0
Michael Pope <mpope.cr@gmail.com>
2015-04-08 19:16:46 -0400

    Refactor JobsController tests

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,45 +1,43 @@
 require "rails_helper"
 
 describe JobsController do
-  let(:company) { Fabricate(:company) }
-  let(:user) { company.user }
+  let(:job) { Job.new(Fabricate.attributes_for(:job)) }
+
+  before { set_current_user(job.company.user) }
 
   context "#create" do
     it "creates a Job" do
       pre_count = Job.count
-      session[:user_id] = user.id
-      post :create, company_id: company.id, job: Fabricate.build(:job, company: company).attributes
+      post :create, company_id: job.company_id, job: job.attributes
       expect(Job.count).to eq(pre_count + 1)
     end
 
     it_behaves_like "require_sign_in" do
-      let(:action) { post :create, company_id: company.id, job: Fabricate.build(:job, company: company).attributes }
+      let(:action) { post :create, company_id: job.company_id, job: job.attributes }
     end
   end
 
   context "#new" do
     it "assigns @company" do
-      session[:user_id] = user.id
-      get :new, company_id: company.id
+      get :new, company_id: job.company_id
       expect(assigns[:job]).to be_instance_of(Job)
     end
 
     it_behaves_like "require_sign_in" do
-      let(:action) { get :new, company_id: company.id }
+      let(:action) { get :new, company_id: job.company_id }
     end
   end
 
   context "#show" do
-    let(:job) { Fabricate(:job, company: company) }
+    before { job.save }
 
     it "assigns @show" do
-      session[:user_id] = company.user.id
-      get :show, company_id: company.id, id: job.id
+      get :show, company_id: job.company_id, id: job.id
       expect(assigns[:job]).to eq(job)
     end
 
     it_behaves_like "require_sign_in" do
-      let(:action) { get :show, company_id: company.id, id: job.id }
+      let(:action) { get :show, company_id: job.company.id, id: job.id }
     end
   end
 end
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -2,6 +2,10 @@ def clear_current_user
   session[:user_id] = nil
 end
 
+def set_current_user(user = Fabricate(:user))
+  session[:user_id] = user.id
+end
+
 def sign_in(user = Fabricate(:user))
   visit sign_in_path
   fill_in :email,    with: user.email
