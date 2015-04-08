45869e6b708a414215145d3b480f7124afe6de84
Michael Pope <mpope.cr@gmail.com>
2015-04-08 18:14:26 -0400

    Refactor company creation test

--- spec/features/company_spec.rb
+++ spec/features/company_spec.rb
@@ -2,16 +2,14 @@ require "rails_helper"
 
 feature "Company" do
   context "creation" do
-    let(:company)  { Fabricate.build(:company, name: Faker::Company.name) }
-    let(:password) { Faker::Internet.password }
-    let(:user)     { Fabricate(:user, password: password) }
+    let(:company)  { Fabricate.build(:company) }
+
+    background do
+      sign_in
+      click_link("New Company")
+    end
 
     scenario "succeeds w/ valid input" do
-      visit sign_in_path
-      fill_in :email, with: user.email
-      fill_in :password, with: password
-      click_button("Submit")
-      click_link("New Company")
       fill_in :company_name, with: company.name
       click_button("Submit")
       expect(page).to have_content(company.name)
