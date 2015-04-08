cf55ca964ac9da6dc545a0af4e85de130df450e2
Michael Pope <mpope.cr@gmail.com>
2015-04-08 18:01:54 -0400

    Add sign_in macro for Rspec && refactor sign_in test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,13 +1,8 @@
 require "rails_helper"
 
 feature "Sign In" do
-  let(:user) { Fabricate(:user) }
-
   scenario "success" do
-    visit sign_in_path
-    fill_in "email", with: user.email
-    fill_in "password", with: user.password
-    click_button("Submit")
+    sign_in
     expect(page).to have_content("You are now signed in")
   end
 end
