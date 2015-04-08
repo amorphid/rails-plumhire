d2def09447eea4e6f4c42ddf790c4510e6762f3a
Michael Pope <mpope.cr@gmail.com>
2015-04-08 18:06:38 -0400

    Refactor sign up test

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,17 +2,16 @@ require "rails_helper"
 
 feature "Sign up" do
   let(:user) { Fabricate.build(:user) }
-  let!(:email) { user.email }
   let(:password) { user.password }
 
   scenario "success" do
     visit sign_up_path
-    fill_in "user_email", with: email
+    fill_in "user_email", with: user.email
     fill_in "user_password", with: password
     click_button("Submit")
-    expect(page).to have_content("Email sent to #{email}")
-    open_email(email)
+    expect(page).to have_content("Email sent to #{user.email}")
+    open_email(user.email)
     current_email.click_link "click here"
-    expect(page).to have_content("Email #{email} confirmed")
+    expect(page).to have_content("Email #{user.email} confirmed")
   end
 end
