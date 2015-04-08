d1b4a594deace46cd795c3cebbd9e323d3a25a4c
Michael Pope <mpope.cr@gmail.com>
2015-04-08 15:28:46 -0400

    Add route test for GET /companies/:company_id/jobs/:id

--- spec/routing/jobs_controller_spec.rb
+++ spec/routing/jobs_controller_spec.rb
@@ -9,6 +9,15 @@ describe JobsController do
     )
   end
 
+  it "GET /companies/:company_id/jobs/:id" do
+    expect(get: "/companies/:company_id/jobs/:id").to route_to(
+      controller: "jobs",
+      company_id: ":company_id",
+      id:         ":id",
+      action:     "show"
+    )
+  end
+
   it "POST /companies/:company_id/jobs" do
     expect(post: "/companies/:company_id/jobs").to route_to(
       controller: "jobs",
