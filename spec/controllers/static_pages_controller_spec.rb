8f10f4f1cc5bc88bf09f86d287f446a9635ccb87
Michael Pope <mpope.cr@gmail.com>
2015-04-08 21:05:10 -0400

    Refactor StaticPagesController tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,26 +1,24 @@
 require "rails_helper"
 
 describe StaticPagesController do
+  let(:user) { User.new(Fabricate.attributes_for(:user)) }
+
   context "#email_sent" do
-    let(:user) { Fabricate(:user) }
+    before { user.save }
 
     it "assigns @user" do
       get :email_sent, id: user.id, email: user.email
       expect(assigns[:user]).to be_instance_of(User)
     end
 
-    it "redirects if signed in" do
-      session[:user_id] = user.id
-      get :email_sent, id: user.id
-      expect(response).to redirect_to(home_path)
+    it_behaves_like "redirects_to_home_page_when_signed_in" do
+      let(:action) { get :sign_in }
     end
   end
 
   context "#home" do
-    it "redirects if not signed in" do
-      session[:user_id] = nil
-      get :home
-      expect(response).to redirect_to(sign_in_path)
+    it_behaves_like "require_sign_in" do
+      let(:action) { get :home }
     end
   end
 
@@ -35,10 +33,8 @@ describe StaticPagesController do
       expect(flash[:success]).not_to be_present
     end
 
-    it "redirects if signed in" do
-      session[:user_id] = Fabricate(:user).id
-      get :sign_in
-      expect(response).to redirect_to(home_path)
+    it_behaves_like "redirects_to_home_page_when_signed_in" do
+      let(:action) { get :sign_in }
     end
   end
 
@@ -48,10 +44,8 @@ describe StaticPagesController do
       expect(assigns[:user]).to be_instance_of(User)
     end
 
-    it "redirects if signed in" do
-      session[:user_id] = Fabricate(:user).id
-      get :sign_up
-      expect(response).to redirect_to(home_path)
+    it_behaves_like "redirects_to_home_page_when_signed_in" do
+      let(:action) { get :sign_up }
     end
   end
 end
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,3 +1,11 @@
+shared_examples "redirects_to_home_page_when_signed_in" do
+  it "redirects to the home page when signed in" do
+    session[:user_id] = Fabricate(:user).id
+    action
+    expect(response).to redirect_to(home_path)
+  end
+end
+
 shared_examples "require_sign_in" do
   it "redirects to the sign in page" do
     clear_current_user
