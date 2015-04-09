eef60efed3d7abcc272e285fc4e769f101ef13a6
Michael Pope <mpope.cr@gmail.com>
2015-04-08 21:57:45 -0400

    Refactor UsersController#email

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,26 @@ require "rails_helper"
 
 describe UsersController do
   context "#create" do
-    let(:user_attr) { Fabricate.attributes_for(:user) }
+    let(:user) { Fabricate.build(:user) }
 
     it "sends an email w/ valid input" do
       pre_count = ActionMailer::Base.deliveries.count
-      post :create, user: user_attr
+      post :create, user: user.attributes
       expect(ActionMailer::Base.deliveries.count).to eq(pre_count + 1)
     end
 
     it "creates a user w/ valid input" do
       pre_count = User.count
-      post :create, user: user_attr
+      post :create, user: user.attributes
       expect(User.count).to eq(pre_count + 1)
     end
 
     it "redirects w/ valid input" do
-      post :create, user: user_attr
-      user = User.find_by(email: user_attr[:email])
-      expect(response).to redirect_to(email_sent_path(user, { email: user.email }))
+      post :create, user: user.attributes
+      expect(response).to redirect_to(email_sent_path(
+        User.find_by(email: user.email),
+        { email: user.email }
+      ))
     end
   end
 end
