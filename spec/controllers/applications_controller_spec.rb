b107eef1a4af9212bee6a9d3fa10a0fa8ab6a0a6
Michael Pope <mpope.cr@gmail.com>
2015-04-10 11:22:22 -0400

    Add controller tests for ApplicationControllers#create

--- spec/controllers/applications_controller_spec.rb
+++ spec/controllers/applications_controller_spec.rb
@@ -5,6 +5,22 @@ describe ApplicationsController do
 
   before { set_session_user_id }
 
+  context "create" do
+    it "creates an Application" do
+      pre_count = Application.count
+      post(
+        :create,
+        job_id: application.job_id,
+        application: application.attributes
+      )
+      expect(Application.count).to eq(pre_count + 1)
+    end
+
+    it_behaves_like "require_sign_in" do
+      let(:action) { post(:create, job_id: application.job_id) }
+    end
+  end
+
   context "new" do
     it "assigns @application" do
       get :new, job_id: application.job_id
@@ -12,7 +28,7 @@ describe ApplicationsController do
     end
 
     it_behaves_like "require_sign_in" do
-      let(:action) { get :new, job_id: application.job.id }
+      let(:action) { get :new, job_id: application.job_id }
     end
   end
 end
