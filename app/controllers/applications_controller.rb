d3faf36cf83b71ac7690c67102099abd1c7f18c5
Michael Pope <mpope.cr@gmail.com>
2015-04-10 11:29:12 -0400

    Implement ApplicationsController#create

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,9 +2,17 @@ class ApplicationsController < ApplicationController
   before_filter :authorize_user!
 
   def create
+    application = Application.create(application_params)
+    redirect_to job_application_path(application.job_id, application)
   end
 
   def new
     @application = Application.new(job_id: params[:job_id])
+  end
+
+  private
+
+  def application_params
+    params.require(:application).permit(:email, :job_id, :name, :phone)
   end
 end
