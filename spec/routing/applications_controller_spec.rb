cc75a916441dbc57a50e30cae0be4ef487f9f5cd
Michael Pope <mpope.cr@gmail.com>
2015-04-09 20:50:12 -0400

    Add route test for new applications

--- spec/routing/applications_controller_spec.rb
+++ spec/routing/applications_controller_spec.rb
@@ -1,4 +1,11 @@
-# require "rails_helper"
+require "rails_helper"
 
-# describe ApplicationsHelper do
-# end
+describe ApplicationsController do
+  it do
+    expect(get: "/jobs/:job_id/applications/new").to route_to(
+      controller: "applications",
+      job_id:     ":job_id",
+      action:     "new"
+    )
+  end
+end
