f8c0b472252477570dfca86af078ca4f1c9eb8c9
Michael Pope <mpope.cr@gmail.com>
2015-04-10 10:49:15 -0400

    Add create to ApplicationsController

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationsController < ApplicationController
   before_filter :authorize_user!
 
+  def create
+  end
+
   def new
     @application = Application.new(job_id: params[:job_id])
   end
