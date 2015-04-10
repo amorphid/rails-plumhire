e9c0eb717fefad6c3538062c9d10961332106822
Michael Pope <mpope.cr@gmail.com>
2015-04-10 13:44:18 -0400

    Add route test for JobsController#edit

--- spec/routing/jobs_controller_spec.rb
+++ spec/routing/jobs_controller_spec.rb
@@ -2,6 +2,15 @@ require "rails_helper"
 
 describe JobsController do
   it do
+    expect(get: "/companies/:company_id/jobs/:id/edit").to route_to(
+      controller: "jobs",
+      company_id: ":company_id",
+      id:         ":id",
+      action:     "edit"
+    )
+  end
+
+  it do
     expect(get: "/companies/:company_id/jobs/new").to route_to(
       controller: "jobs",
       company_id: ":company_id",
