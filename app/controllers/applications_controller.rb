6419699661aa5bf9c9df16759fb01010a09b9175
Michael Pope <mpope.cr@gmail.com>
2015-04-09 21:12:22 -0400

    Implement ApplicationsController#new

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationsController < ApplicationController
+  before_filter :authorize_user!
+
   def new
+    @application = Application.new(job_id: params[:job_id])
   end
 end
--- spec/controllers/applications_controller_spec.rb
+++ spec/controllers/applications_controller_spec.rb
@@ -3,9 +3,11 @@ require "rails_helper"
 describe ApplicationsController do
   let(:application) { build_with_attributes(Application) }
 
+  before { set_current_user(application.job.company.user) }
+
   context "new" do
     it "assigns @application" do
-      get :new, job_id: application.job.id
+      get :new, job_id: application.job_id
       expect(assigns[:application]).to be_instance_of(Application)
     end
 
