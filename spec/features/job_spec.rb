8584e854f8524611f24bbfb0131ac79612e5332c
Michael Pope <mpope.cr@gmail.com>
2015-04-10 13:38:59 -0400

    Add feature test for editing job

--- spec/features/job_spec.rb
+++ spec/features/job_spec.rb
@@ -1,11 +1,12 @@
 require "rails_helper"
 
 feature "Job" do
-  let(:job) { build_with_attributes(Job) }
+  background { sign_in }
 
   context "creation" do
+    let(:job) { build_with_attributes(Job) }
+
     background do
-      sign_in
       visit company_path(job.company)
       click_link("New Job")
     end
@@ -16,6 +17,23 @@ feature "Job" do
       click_button("Submit")
       expect(page).to have_content(job.title)
       expect(page).to have_content(job.description)
+    end
+  end
+
+  context "edit" do
+    let(:job) { Fabricate(:job) }
+
+    background do
+      visit company_job_path(job.company, job)
+      click_link("Edit Job")
+    end
+
+    scenario "succeeds w/ valid input" do
+      new_title = job.title + ", the other one"
+
+      fill_in :job_title, with: new_title
+      click_button("Submit")
+      expect(page).to have_content(new_title)
     end
   end
 end
